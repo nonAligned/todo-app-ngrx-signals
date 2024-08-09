b63244e41fc863852c8fdf7e0c6064a3b69dd687
Momcilo Savic <savic227@gmail.com>
2024-08-09 16:37:34 +0200

    Create signal store with loadAll method, create todos-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosStore } from './store/todos.store';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'todo-app';
+export class AppComponent implements OnInit {
+
+  store = inject(TodosStore);
+
+  ngOnInit(): void {
+    this.loadTodos().then(() => console.log("Todos Loaded!"));
+  }
+
+  async loadTodos() {
+    await this.store.loadAll();
+  }
 }
