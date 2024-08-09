b414376cad4b45daa24044ae50c1cae6f785469d
Momcilo Savic <savic227@gmail.com>
2024-08-09 16:06:51 +0200

    Create todo data model and mock data

---
+++ src/app/model/mock-data.ts
@@ -0,0 +1,8 @@
+import { Todo } from "./todo.model";
+
+export const TODOS: Todo[] = [
+    {id: "1", title: "Learn Angular", completed: false},
+    {id: "2", title: "Build and Angular app", completed: false},
+    {id: "3", title: "Learn Node", completed: false},
+    {id: "4", title: "Learn React", completed: false},
+];
