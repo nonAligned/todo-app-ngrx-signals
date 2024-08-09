d596c27761808ce3d3fdad7ecc69984fed85714a
Momcilo Savic <savic227@gmail.com>
2024-08-09 17:11:46 +0200

    Display data in todos-list component, add loading spinner when loading data

--- src/app/todos-list/todos-list.component.ts
+++ src/app/todos-list/todos-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
+import { MatListOption, MatSelectionList } from '@angular/material/list';
+import { TodosStore } from '../store/todos.store';
 
 @Component({
   selector: 'todos-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField, 
+    MatLabel, 
+    MatInput, 
+    MatIcon,
+    MatSuffix,
+    MatButtonToggleGroup,
+    MatButtonToggle,
+    MatSelectionList,
+    MatListOption
+  ],
   templateUrl: './todos-list.component.html',
   styleUrl: './todos-list.component.scss'
 })
 export class TodosListComponent {
-
+  store = inject(TodosStore);
 }
