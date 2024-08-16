c571488474193e35eee27f3732a120735c247226
Momcilo Savic <savic227@gmail.com>
2024-08-16 14:40:41 +0200

    Generate register component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './core/login/login.component';
 import { TodosComponent } from './todo/todos/todos.component';
 import { authGuard } from './shared/auth.guard';
+import { RegisterComponent } from './core/register/register.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'todos', component: TodosComponent, canActivate: [authGuard] },
     { path: "", redirectTo: "todos", pathMatch: "full"}
 ];
